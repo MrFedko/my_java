aab6e0a057e519924da536eead5f99e27e0c2ec2
MrFedko <miklerst@mail.ru>
2022-05-23 15:15:54 +0300

    нахождение n-ого треугольного числа

---
+++ home_work/triangle.java
@@ -0,0 +1,17 @@
+package home_work;
+import java.util.Scanner;
+
+
+public class triangle {
+    public static  void main(String[] args){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("int a number of Nombre triangulaire: ");
+        int i = iScanner.nextInt();
+        System.out.printf("Nombre triangulaire: %d\n", giveMeNumber(i));
+        iScanner.close();
+    }
+
+    public static int giveMeNumber(int a){
+        return (a * (a + 1)) / 2; 
+    }
+}
