77ca2358476effc4c5279b69a200839efe9eafc5
MrFedko <miklerst@mail.ru>
2022-06-09 15:43:25 +0300

    bubble sort

---
+++ bubble_sort/bubbleSort.java
@@ -0,0 +1,30 @@
+package bubble_sort;
+
+import sorting.Sorting;
+
+public class bubbleSort extends Sorting {
+
+    static int[] array;
+
+    public bubbleSort(int[] myArray) {
+        array = myArray;
+    }
+
+    public int[] bubbles(int[] array) {
+        int length = array.length;
+        for (int i = 0; i + 1 < length; i++) {
+            for (int j = 0; j + 1 < length - i; j++) {
+                if (array[j + 1] < array[j])
+                    swap(array, j, j + 1);
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = getFromUser();
+        bubbleSort mySort = new bubbleSort(myArray);
+        mySort.bubbles(myArray);
+        printArray(bubbleSort.array);
+    }
+}
