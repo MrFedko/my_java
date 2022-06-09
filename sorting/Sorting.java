4c4c18d0cfd44d49f845910ee4090a999d00f863
MrFedko <miklerst@mail.ru>
2022-06-09 15:30:47 +0300

    create getFromUser

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -1,4 +1,8 @@
 package sorting;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Sorting {
     public static void swap(int[]array, int firstIndex, int secondIndex){
         int temp = array[firstIndex];
@@ -10,5 +14,12 @@ public class Sorting {
         for (int i = 0; i < length; ++i)
             System.out.print(array[i] + " ");
         System.out.println();
+    }
+    public static int[] getFromUser(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Enter numbers separated by spaces: ");
+        int myArray[] = Stream.of(iScanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        iScanner.close();
+        return myArray;
     }
 }
